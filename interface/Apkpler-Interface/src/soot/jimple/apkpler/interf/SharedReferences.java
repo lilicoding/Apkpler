59e7fdd21a8671bd74d725bb3c4ebb3ccfebf864
Li <joeyli1989@gmail.com>
2014-11-04 15:38:33 +0100

    add comments to SharedPreferences

--- interface/Apkpler-Interface/src/soot/jimple/apkpler/interf/SharedReferences.java
+++ interface/Apkpler-Interface/src/soot/jimple/apkpler/interf/SharedReferences.java
@@ -2,6 +2,11 @@ package soot.jimple.apkpler.interf;
 
 import java.util.HashMap;
 
+/**
+ * This class is designed to share variables between different plugins.
+ * For example, one plugin can put some infos (key-value) to #refs# and 
+ * later other plugin can read that infos.
+ */
 public class SharedReferences 
 {
 	public static HashMap<String, String> refs = new HashMap<String, String>();
