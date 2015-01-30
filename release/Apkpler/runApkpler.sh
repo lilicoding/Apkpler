555b83bb5420c006b17432afc3cbefd7a8bbb0bc
Li Li Li.Li@uni.lu <lli@node-100.gaia-cluster.uni.lux>
2015-01-30 16:14:19 +0100

    update the script

--- release/Apkpler/runApkpler.sh
+++ release/Apkpler/runApkpler.sh
@@ -1,7 +1,15 @@
 #! /bin/sh
 
 APP=$1
-
 APP_NAME=`basename $APP`
 
-timeout $DUR java -Xmx8G -jar Apkpler.jar $APP > sys_out/$APP_NAME.txt 2>&1 
+ANDROID_PLATFORMS=/work/users/lli/gitbitbucket/android-platforms
+
+DUR=600
+
+timeout $DUR java -Xmx8G -jar Apkpler.jar $APP $ANDROID_PLATFORMS > sys_out/$APP_NAME.txt 2>&1 
+
+BYTE_SIZE=`unzip -l $APP | grep classes.dex | awk '{print $1}'`
+#TIME=`grep "\[Apkpler\]====>" sys_out/$APP_NAME.txt | sed 's@\[Apkpler\]====>@@'`
+
+echo "====>"$BYTE_SIZE >> sys_out/$APP_NAME.txt
